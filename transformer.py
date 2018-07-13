0c42876a8fbbf1631bfc4a9a4a5a63b64f5d6c61
Joohong Lee <roomylee@naver.com>
2018-07-13 18:58:03 +0900

    implement masked Multi-head att & decoder

--- transformer.py
+++ transformer.py
@@ -26,51 +26,81 @@ class Transformer:
 
         with tf.name_scope("encoder"):
             # Embeddings
-            with tf.device('/cpu:0'), tf.name_scope("embedding"):
-                self.W_embedding = tf.get_variable("W_embedding", dtype=tf.float32,
-                                                   shape=[self.source_vocab_size, self.hidden_size],
-                                                   initializer=tf.contrib.layers.xavier_initializer())
-                self.embedded_chars = tf.nn.embedding_lookup(self.W_embedding, self.input_source)
+            with tf.device('/cpu:0'), tf.name_scope("source_embedding"):
+                self.W_source_embedding = tf.get_variable("W_source_embedding", dtype=tf.float32,
+                                                          shape=[self.source_vocab_size, self.hidden_size],
+                                                          initializer=initializer)
+                self.source_embedded_chars = tf.nn.embedding_lookup(self.W_source_embedding, self.input_source)
 
 
             with tf.name_scope("positional-encoding"):
-                # First part of the PE function: sin and cos argument
-                self.position_enc = np.array([[pos / (10000 ** (2*i/self.hidden_size))
-                                               for i in range(self.hidden_size)]
-                                              for pos in range(self.sequence_length)])
-                # Second part, apply the cosine to even columns and sin to odds.
-                self.position_enc[:, 0::2] = np.sin(self.position_enc[:, 0::2])  # dim 2i
-                self.position_enc[:, 1::2] = np.cos(self.position_enc[:, 1::2])  # dim 2i+1
-                self.position_enc = tf.convert_to_tensor(self.position_enc, dtype=tf.float32)
-
-                self.enc = self.embedded_chars + self.position_enc
-
-                self.temp = self.enc
+                self.enc = self.source_embedded_chars + self.position_encoding()
 
             for i in range(self.num_stack):
-                with tf.name_scope("stacked-layer-{}".format(i)):
+                with tf.name_scope("encoder-block-{}".format(i)):
                     # Multihead Attention (self attention)
-                    self.h_att = self.multihead_attention(query=self.enc,
-                                                          key=self.enc,
-                                                          value=self.enc)
-                    # Position-wise Feed Forward
-                    self.h_ff = self.feedforward(self.enc)
+                    self.mh = self.multihead_attention(query=self.enc,
+                                                       key=self.enc,
+                                                       value=self.enc)
+                    self.mh = tf.contrib.layers.layer_norm(self.enc + self.mh)
 
-                    self.enc = tf.contrib.layers.layer_norm(self.enc + self.h_ff)
+                    # Position-wise Feed Forward
+                    self.ff = self.feedforward(self.mh)
+                    self.enc = tf.contrib.layers.layer_norm(self.mh + self.ff)
 
         with tf.name_scope("decoder"):
-            pass
+            # Embeddings
+            with tf.device('/cpu:0'), tf.name_scope("target_embedding"):
+                self.W_target_embedding = tf.get_variable("W_target_embedding", dtype=tf.float32,
+                                                          shape=[self.target_vocab_size, self.hidden_size],
+                                                          initializer=initializer)
+                self.target_embedded_chars = tf.nn.embedding_lookup(self.W_target_embedding, self.input_target)
+
+            with tf.name_scope("positional-encoding"):
+                self.dec = self.target_embedded_chars + self.position_encoding()
+
+            for i in range(self.num_stack):
+                with tf.name_scope("decoder-block-{}".format(i)):
+                    # Masked Multihead Attention (self attention)
+                    self.mask_mh = self.multihead_attention(query=self.dec,
+                                                            key=self.dec,
+                                                            value=self.dec,
+                                                            masked=True)
+                    self.mask_mh = tf.contrib.layers.layer_norm(self.dec + self.mask_mh)
+
+                    # Multihead Attention (attention with encoder)
+                    self.mh = self.multihead_attention(query=self.mask_mh,
+                                                       key=self.enc,
+                                                       value=self.enc)
+                    self.mh = tf.contrib.layers.layer_norm(self.mask_mh + self.mh)
+
+                    # Position-wise Feed Forward
+                    self.ff = self.feedforward(self.mh)
+                    self.dec = tf.contrib.layers.layer_norm(self.mh + self.ff)
 
 
+    def position_encoding(self):
+        # First part of the PE function: sin and cos argument
+        position_enc = np.array([[pos / (10000 ** (2 * i / self.hidden_size))
+                                       for i in range(self.hidden_size)]
+                                      for pos in range(self.sequence_length)])
+        # Second part, apply the cosine to even columns and sin to odds.
+        position_enc[:, 0::2] = np.sin(position_enc[:, 0::2])  # dim 2i
+        position_enc[:, 1::2] = np.cos(position_enc[:, 1::2])  # dim 2i+1
+        position_enc = tf.convert_to_tensor(position_enc, dtype=tf.float32)
+        return position_enc
 
-
-    def scaled_dot_product_attention(self, query, key, value, scaling_factor):
+    def scaled_dot_product_attention(self, query, key, value, scaling_factor, masked):
         QK_T = tf.matmul(query, tf.transpose(key, [0, 2, 1]))
+        if masked:
+            mask = tf.ones([self.hidden_size, self.hidden_size])
+            mask = tf.contrib.linalg.LinearOperatorTriL(mask, tf.float32).to_dense()
+            QK_T = tf.matmul(QK_T, mask)
         attention = tf.nn.softmax(QK_T * scaling_factor)
         att_V = tf.matmul(attention, value)
         return att_V
 
-    def multihead_attention(self, query, key, value):
+    def multihead_attention(self, query, key, value, masked=False):
         attentions = []
         for i in range(self.num_head):
             key_dim = int(self.hidden_size / self.num_head)
@@ -87,7 +117,9 @@ class Transformer:
             b_v = tf.Variable(tf.constant(0.1, shape=[key_dim]))
             V = tf.tensordot(value, W_v, [[2], [0]]) + b_v
 
-            dot_att = self.scaled_dot_product_attention(Q, K, V, scaling_factor=tf.sqrt(self.num_head / self.hidden_size))
+            dot_att = self.scaled_dot_product_attention(Q, K, V,
+                                                        scaling_factor=tf.sqrt(self.num_head / self.hidden_size),
+                                                        masked=masked)
 
             attentions.append(dot_att)
 
@@ -99,7 +131,6 @@ class Transformer:
 
         return output
 
-
     def feedforward(self, x):
         W1 = tf.Variable(tf.truncated_normal([1, self.hidden_size, self.ff_hidden_size], stddev=0.1))
         b1 = tf.Variable(tf.constant(0.1, shape=[self.ff_hidden_size]))
@@ -109,6 +140,6 @@ class Transformer:
         W2 = tf.Variable(tf.truncated_normal([1, self.ff_hidden_size, self.hidden_size], stddev=0.1))
         b2 = tf.Variable(tf.constant(0.1, shape=[self.hidden_size]))
         conv2 = tf.nn.conv1d(h1, W2, stride=1, padding='VALID')
-        h2 = tf.nn.relu(tf.nn.bias_add(conv2, b2))
+        h2 = tf.nn.bias_add(conv2, b2)
 
         return h2
